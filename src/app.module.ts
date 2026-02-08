89d640476ea39ce97f647c5f68a7d40f42178a49
byeolki <byeolki7007@gmail.com>
2026-02-08 13:37:18 +0900

    refactor: move auth and profile to modules directory, simplify schema comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
-import { AuthModule } from './auth/auth.module';
-import { ProfileModule } from './profile/profile.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { ProfileModule } from './modules/profile/profile.module';
 
 @Module({
   imports: [
